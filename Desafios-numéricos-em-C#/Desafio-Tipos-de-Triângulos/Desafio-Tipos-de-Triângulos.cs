242b6203664bbb5baef533e8620a66c05b95656c
StephHoel <steph.hoel@gmail.com>
2021-10-10 18:49:31 -0300

    Adicionando Desafio-Tipos-de-Triângulos

---
+++ Desafios-numéricos-em-C#/Desafio-Tipos-de-Triângulos/Desafio-Tipos-de-Triângulos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class MinhaClasse
+{
+   static void Main()
+    {
+        string[] s = Console.ReadLine().Split(' ');
+        double a = double.Parse(s[0]);
+        double b = double.Parse(s[1]);
+        double c = double.Parse(s[2]);
+
+        double a2 = Math.Max(a, Math.Max(b, c));
+        double c2 = Math.Min(a, Math.Min(b, c));
+        double b2 = a + b + c - a2 - c2;
+
+        if (a2 >= b2 + c2)
+            Console.WriteLine("NAO FORMA TRIANGULO");
+        else if (Math.Pow(a2, 2) == Math.Pow(b2, 2) + Math.Pow(c2, 2))
+            Console.WriteLine("TRIANGULO RETANGULO");
+        else if (Math.Pow(a2, 2) > Math.Pow(b2, 2) + Math.Pow(c2, 2))
+            Console.WriteLine("TRIANGULO OBTUSANGULO");
+        else if (Math.Pow(a2, 2) < Math.Pow(b2, 2) + Math.Pow(c2, 2))
+            Console.WriteLine("TRIANGULO ACUTANGULO");
+        if (a2 == b2 && b2 == c2)
+            Console.WriteLine("TRIANGULO EQUILATERO");
+        if (a2 == b2 && b2 != c2 || b2 == c2 && c2 != a2)
+            Console.WriteLine("TRIANGULO ISOSCELES");
+
+        Console.ReadLine();
+    }
+
+}
