f09d0f5135178db27e70573dc71a603d30977297
StephHoel <steph.hoel@gmail.com>
2021-10-10 18:24:22 -0300

    Adicionando Desafio-O-Maior

---
+++ Desafios-numéricos-em-C#/Desafio-O-Maior/Desafio-O-Maior.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MinhaClasse
+{
+   static void Main()
+    {
+        string[] vet = Console.ReadLine().Split(' ');
+        int a = int.Parse(vet[0]);
+        int b = int.Parse(vet[1]);
+        int c = int.Parse(vet[2]);
+
+        int MAIORAB = (a + b + Math.Abs(a - b)) / 2;
+
+        int MAIOR = (MAIORAB + c + Math.Abs(MAIORAB - c)) / 2;
+
+        Console.WriteLine($"{MAIOR} eh o maior");
+    }
+
+}
