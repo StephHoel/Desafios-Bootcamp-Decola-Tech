edcdf166f4281f767f4751a58b991c61a707ec9c
StephHoel <steph.hoel@gmail.com>
2021-10-10 18:38:13 -0300

    Adicionando Desafio-Validação-de-Nota

---
+++ Desafios-numéricos-em-C#/Desafio-Validação-de-Nota/Desafio-Validação-de-Nota.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MinhaClasse
+{
+   static void Main()
+    {
+        decimal entrada = 0;
+        decimal soma = 0;
+        decimal i = 0;
+
+        do
+        {
+            entrada = Convert.ToDecimal(Console.ReadLine());
+
+            if (entrada >= 0 && entrada <= 10)
+            {
+                soma += entrada;
+                i++;
+            }
+            else Console.WriteLine("nota invalida");
+        }
+        while (i < 2);
+
+        Console.WriteLine("media = " + soma / 2);
+    }
+
+}
