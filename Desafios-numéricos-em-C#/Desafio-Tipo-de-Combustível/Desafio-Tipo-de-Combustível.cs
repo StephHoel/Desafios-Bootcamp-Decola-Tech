64cc7ccb24725e1f130c8529ee9cae7614c778a2
StephHoel <steph.hoel@gmail.com>
2021-10-10 17:41:17 -0300

    Adicionando Desafio-Tipo-de-Combustível

---
+++ Desafios-numéricos-em-C#/Desafio-Tipo-de-Combustível/Desafio-Tipo-de-Combustível.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class MinhaClasse
+{
+   static void Main()
+    {
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+        int i;
+
+        do
+        {
+
+            i = Convert.ToInt32(Console.ReadLine());
+
+            switch (i)
+            {
+                case 1:
+                alcool++;
+                continue;
+                case 2:
+                gasolina++;
+                continue;
+                case 3:
+                diesel++;
+                continue;
+                case 4:
+                Console.WriteLine("MUITO OBRIGADO");
+                Console.WriteLine("Alcool: " + alcool);
+                Console.WriteLine("Gasolina: " + gasolina);
+                Console.WriteLine("Diesel: " + diesel);
+                break;
+                default:
+                continue;
+            }
+        }
+
+        while (i != 4);
+    }
+
+}
