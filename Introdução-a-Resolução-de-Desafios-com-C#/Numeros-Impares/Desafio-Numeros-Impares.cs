c5b1f0606530a3d4ebfb922fa9b9007e14dfb650
StephHoel <steph.hoel@gmail.com>
2021-10-10 17:09:40 -0300

    Adicionando Desafio-Numeros-Impares

---
+++ Introdução-a-Resolução-de-Desafios-com-C#/Numeros-Impares/Desafio-Numeros-Impares.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MinhaClasse
+{
+
+   static void Main()
+    {
+        int n;
+        n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i <= n; i++) 
+        {
+            if ((i % 2) != 0)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        Console.ReadLine();
+    }
+
+}
