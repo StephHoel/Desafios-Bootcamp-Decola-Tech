a5c9e7ab5913d6a0d67a4cf9e8de516ac0c56811
StephHoel <steph.hoel@gmail.com>
2021-10-10 17:15:13 -0300

    Adicionando Desafio-Conversao-de-Tempo

---
+++ Introdução-a-Resolução-de-Desafios-com-C#/Desafio-Conversao-de-Tempo/Desafio-Conversao-de-Tempo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MinhaClasse
+{
+   static void Main()
+    {
+        var timeInSeconds = int.Parse(Console.ReadLine());
+
+        var hours = timeInSeconds / (60 * 60);
+        timeInSeconds %= (60 * 60);
+        var minutes = timeInSeconds / 60;
+        var seconds = timeInSeconds % 60;
+
+        Console.WriteLine($"{hours}:{minutes}:{seconds}");
+    }
+
+}
