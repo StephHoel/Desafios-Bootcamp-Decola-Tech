ff60c18c4863ea93a0445ccdc5221f50a62c68e4
StephHoel <steph.hoel@gmail.com>
2021-10-10 15:56:05 -0300

    Desafio Soma

---
+++ Primeiros-Desafios-Matemáticos-em-C#/Desafio-Soma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Declarando Variáveis e Recebendo os valores
+        int A = Convert.ToInt32(Console.ReadLine());
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        //Fazendo a lógica de soma
+        int SOMA = A + B;
+
+        //Imprimindo a resposta da soma
+        Console.WriteLine("SOMA = {0}", SOMA);
+    }
+}
