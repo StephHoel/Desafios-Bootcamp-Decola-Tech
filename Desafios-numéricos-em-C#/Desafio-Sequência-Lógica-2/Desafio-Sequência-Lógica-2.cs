8a2062cc4d6d1c79f318c1434b942c4f295e83b7
StephHoel <steph.hoel@gmail.com>
2021-10-10 18:56:55 -0300

    Adicionando Desafio-Sequência-Lógica-2

---
+++ Desafios-numéricos-em-C#/Desafio-Sequência-Lógica-2/Desafio-Sequência-Lógica-2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class MinhaClasse
+{
+   static void Main()
+    {
+        string[] vet = Console.ReadLine().Split(' ');
+
+        int a = int.Parse(vet[0]);
+        int b = int.Parse(vet[1]);
+
+        for (int i = 1, z = 1; i <= b; i++)
+        {
+            Console.Write(i);
+            if (z == a)
+            {
+                Console.WriteLine();
+                z = 1;
+            }
+            else
+            {
+                Console.Write(" ");
+                z++;
+            }
+        }
+    }
+
+}
